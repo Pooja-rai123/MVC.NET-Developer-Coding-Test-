b08044454696d8fa742028247488b9cb47483512
CCI <CCI@DESKTOP-0G2V5NV>
2025-05-20 16:10:21 +0530

    Add project files.

---
+++ MVC.NET Developer Coding Test/Controllers/EmployeeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient; // Key change here
+using System.Data;
+using MVC.NET_Developer_Coding_Test.Models;
+using MVC.NET_Developer_Coding_Test.Models;
+using System.Data.SqlClient;
+
+namespace YourAppName.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            List<string> departments = new List<string>();
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT DISTINCT Department FROM Employees";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    departments.Add(reader["Department"].ToString());
+                }
+            }
+
+            ViewBag.Departments = departments;
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetEmployeesByDepartment(string department)
+        {
+            List<Employee> employees = new List<Employee>();
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT EmployeeId, FirstName, LastName, Salary FROM Employees WHERE Department = @Department";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Department", department);
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    employees.Add(new Employee
+                    {
+                        EmployeeId = Convert.ToInt32(reader["EmployeeId"]),
+                        FirstName = reader["FirstName"].ToString(),
+                        LastName = reader["LastName"].ToString(),
+                        Salary = Convert.ToDecimal(reader["Salary"])
+                    });
+                }
+            }
+
+            return Json(employees);
+        }
+    }
+}
---
+++ MVC.NET Developer Coding Test/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC.NET_Developer_Coding_Test.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
